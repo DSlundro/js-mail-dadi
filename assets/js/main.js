41a5cab8207221ddfa36197e042a0c38bc067615
DSlundro <dragan.savic95@outlook.it>
2022-03-29 16:12:16 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
-const myList = ['one@gmail.com', 'two@gmail.com', 'three@gmail.com', 'four@gmail.com', 'five@gmail.com']
+// impostare una Array (lista delle email consentite per il login)
+let myList = ['one@gmail.com', 'two@gmail.com', 'three@gmail.com', 'four@gmail.com', 'five@gmail.com'];
 
+// chiedere l'imput dall'utente
+let userMail = prompt('Inserisci la tua email')
 console.log(myList)
+
+
+for (let i = 0; i < myList.length; i++){
+    
+        if (myList == userMail){
+            console.log('registrato');
+        }   else if (myList != userMail) {
+            console.log('non registrato');
+        }   else{
+            alert('Devi inserire la tua email')
+        }
+    
+    }
 
 
 
